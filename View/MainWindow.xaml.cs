420e6f583ee0f1c20ac8ab87a815dc608ef7a362
Luan Weber <luanweber98@gmail.com>
2024-08-27 14:59:11 -0300

    separando as classes

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Moving_Image.ViewModels;
+using System.ComponentModel;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Moving_Image
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainViewModel _mainViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowState = WindowState.Normal;
+            _mainViewModel = new MainViewModel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (inputUserPassword.Password == null) { return; }
+            _mainViewModel.EncryptPassword(inputUserPassword.Password, hashedPassword =>
+            {
+                encryptedTextBox.Text = hashedPassword;
+            });
+        }
+
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Moving_Image.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Moving_Image.ViewModels
+{
+    public class MainViewModel
+    {
+        public void EncryptPassword(string password, Action<string> updateEncryptedTextBox)
+        {
+            byte[] passwordByte = Encoding.UTF8.GetBytes(password);
+            byte[] saltByte = PasswordHasher.GenerateSalt(16);
+            byte[] hashedPasswordByte = PasswordHasher.GeneratePasswordHash(passwordByte, saltByte);
+
+            string hashedPasswordString = PasswordHasher.ConvertToBase64String(hashedPasswordByte);
+            updateEncryptedTextBox(hashedPasswordString);
+         }
+
+    }
+}
